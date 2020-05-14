de245bc4335bf110eaaea404f9389d2abebe7176
HegdeShashank <shashankhegde25@gmail.com>
2020-05-14 14:16:38 +0530

    List components added

---
+++ src/Components/list-item.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class ListItem extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListItem;
