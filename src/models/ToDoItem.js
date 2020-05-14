b59ce0b8133415ef454b230e82f602788613fdd0
vrallabandi <vrallabandi@dealersocket.com>
2020-05-14 23:06:02 +0530

    creating reducer for redux

---
+++ src/models/ToDoItem.js
@@ -0,0 +1,13 @@
+export class ToDoItem{
+
+     constructor(id,title,from,to,isToDo,isDeleted){
+        this.id=id,
+        this.title=title,
+        this.from=from,
+        this.to=to,
+        this.isToDo=isToDo,
+        this.isDeleted=isDeleted
+     }
+}
+
+ 
